20206f8670895230586006538cd7a71ee6991e9c
Dmytro Yakovchuk <iknowtooyou@gmail.com>
2025-09-01 11:36:38 +0300

    fixed items 2 and 3 with checklist

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -3,16 +3,21 @@ import React from 'react';
 export const Person = ({ person }) => {
   const { name, age, sex, isMarried, partnerName } = person;
 
+  let partnerInfo = 'I am not married';
+
+  if (isMarried) {
+    if (sex === 'm') {
+      partnerInfo = `${partnerName} is my wife`;
+    } else if (sex === 'f') {
+      partnerInfo = `${partnerName} is my husband`;
+    }
+  }
+
   return (
-  <section className="Person">
+    <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {age && <p className="Person__age">I am {age}</p>}
-      <p className="Person__partner">
-        {isMarried
-      ? sex === 'm'
-      ? `${partnerName} is my wife`
-      : `${partnerName} is my husband`
-      : 'I am not married'}</p>
-      </section>
-      );
+      {age !== undefined && <p className="Person__age">I am {age}</p>}
+      <p className="Person__partner">{partnerInfo}</p>
+    </section>
+  );
 };
